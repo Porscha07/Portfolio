6f83db68c8abb4848881d3e5edeb1f1696f582f2
Porscha07 <porscha.uga@gmail.com>
2017-05-30 18:16:36 -0400

    main page functional

--- scripts.js
+++ scripts.js
@@ -1,4 +1,73 @@
-$(document).ready(function(){
-	
+// function page(){
+// 	$('.js-nav-link').off('click').removeClass('link-selected js-selected');
+// 	$('js-section').show();
+// 	$('js-welcome').addClass('js-selected');
+// }
+
+
+// $(document).ready(function(){
+// 	$('.js-welcome').hover(function(){
+// 		$(this).css("background-color","blue");
+// 	});
+// 	$('.js-about').hover(function(){
+// 		$(this).css("background-color","blue");
+// 	});
+// 	$('.js-projects').hover(function(){
+// 		$(this).css("background-color","blue");
+// 	});
+// 	$('.js-blog').hover(function(){
+// 		$(this).css("background-color","blue");
+// 	});
+// 	$('.js-contact').hover(function(){
+// 		$(this).css("background-color","blue");
+// 	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
+
+  // // Change selected navbar item on touch
+  // if (windowWidth < 1100) {
+  //   $('.js-about').on('click', function() {
+  //     $('html, body').animate({
+  //       scrollTop: $('.about').offset().top
+  //     });
+  //   });
+  //   $('.js-experience').on('click', function() {
+  //     $('html, body').animate({
+  //       scrollTop: $('.experience').offset().top
+  //     });
+  //   });
+  //   $('.js-projects').on('click', function() {
+  //     $('html, body').animate({
+  //       scrollTop: $('.projects').offset().top
+  //     });
+  //   });
+  //   $('.js-contact').on('click', function() {
+  //     $('html, body').animate({
+  //       scrollTop: $('.contact').offset().top
+  //     });
+  //   });
+  // }
+
+
+
+
