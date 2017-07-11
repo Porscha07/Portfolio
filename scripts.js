65b645026c750e9e380d02cd460d105b5e3b10f7
Porscha07 <porscha.uga@gmail.com>
2017-07-11 00:21:54 -0400

    added projects to portfolio

--- scripts.js
+++ scripts.js
@@ -1,33 +1,12 @@
 
 $(document).ready(function(){
-	$('.enter_link').click(function () {
-    $(this).parent('#splashscreen').fadeOut(500);
-	});
-	
-});
 
-   // $('.js-about').on('click', function() {
-   //    $('.skills').animate({down: '300px'})
-   //    });
- //    });
- //    $('.js-experience').on('click', function() {
- //      $('html, body').animate({
- //        scrollTop: $('.js-experience').offset().top
- //      });
- //    });
- //    $('.js-projects').on('click', function() {
- //      $('html, body').animate({
- //        scrollTop: $('.js-projects').offset().top
- //      });
- //    });
- //    $('.js-contact').on('click', function() {
- //      $('html, body').animate({
- //        scrollTop: $('.js-contact').offset().top
- //      });
- //    });
- //  }
-	// };
-// };
+	$(function() {
+		$('')
+	}
+
+});
+  
 
 
 
